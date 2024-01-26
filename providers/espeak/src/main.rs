8aa1de19ab4d090006210a51d0b4df24ce1971cd
Eitan Isaacson <eitan@monotonous.org>
2024-01-26 11:28:30 -0800

    convert espeak

--- providers/espeak/src/main.rs
+++ providers/espeak/src/main.rs
@@ -1,29 +1,24 @@
 use espeaker;
-use futures::executor::block_on;
-use lazy_static::lazy_static;
-use rodio::{OutputStream, Sink};
-use std::collections::hash_map::DefaultHasher;
-use std::collections::HashMap;
 use std::future::pending;
-use std::hash::Hasher;
-use std::sync::Mutex;
 use std::thread;
 use zbus::{dbus_interface, ConnectionBuilder, MessageHeader, Result, SignalContext};
+use zvariant::OwnedFd;
+use std::os::fd::IntoRawFd;
+
+/// No-op.
+macro_rules! assert_initialized_main_thread {
+    () => {};
+}
+
+/// No-op.
+macro_rules! skip_assert_initialized {
+    () => {};
+}
+
+use auto::*;
+mod auto;
 
 const NORMAL_RATE: f32 = 175.0;
-
-struct TaskEntry {
-    _stream: OutputStream,
-    sink: Sink,
-    espeaker: espeaker::Speaker,
-}
-
-unsafe impl Sync for TaskEntry {}
-unsafe impl Send for TaskEntry {}
-
-lazy_static! {
-    static ref TASKS: Mutex<HashMap<u64, TaskEntry>> = Mutex::new(HashMap::new());
-}
 
 pub struct Voice {
     pub name: String,
@@ -37,162 +32,84 @@ impl Speaker {
     fn new() -> Speaker {
         Speaker {}
     }
-
-    fn task_hash(sender: &str, task_id: u64) -> u64 {
-        let mut s = DefaultHasher::new();
-        s.write(sender.as_bytes());
-        s.write(&task_id.to_ne_bytes());
-        s.finish()
-    }
 }
 
 #[dbus_interface(name = "org.freedesktop.Speech.Provider")]
 impl Speaker {
-    async fn get_voices(&self) -> Vec<(String, String, Vec<String>)> {
-        println!("get_voices");
+    async fn get_voices(&self) -> Vec<(String, String, String, Vec<String>)> {
         espeaker::list_voices()
             .into_iter()
             .map(|v| {
                 (
                     v.name,
                     v.identifier,
+                    "audio/x-spiel,format=S16LE,channels=1,rate=22050".to_string(),
                     v.languages.into_iter().map(|l| l.name).collect(),
                 )
             })
             .collect()
     }
 
-    async fn speak(
+    async fn synthesize(
         &mut self,
-        task_id: u64,
+        fd: OwnedFd,
         utterance: &str,
         voice_id: &str,
         pitch: f32,
         rate: f32,
-        volume: f32,
-        #[zbus(header)] header: MessageHeader<'_>,
-        #[zbus(signal_context)] ctxt: SignalContext<'_>,
+        #[zbus(header)] _header: MessageHeader<'_>,
+        #[zbus(signal_context)] _ctxt: SignalContext<'_>,
     ) {
-        println!(
-            "speak '{}' {} {} {}\n  {:?}",
-            utterance,
-            pitch,
-            rate,
-            volume,
-            thread::current()
-        );
-        // println!("header '{:?}'", header);
-        let sender = String::from(header.sender().unwrap().unwrap().to_owned().as_str());
-        let task_key = Speaker::task_hash(sender.as_str(), task_id);
         let s = String::from(utterance);
-        let owned_ctxt = ctxt.into_owned();
-        let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-        let sink = Sink::try_new(&stream_handle).unwrap();
-        let mut espeaker = espeaker::Speaker::new();
-        if !voice_id.is_empty() {
-            let voice = espeaker::list_voices()
-                .into_iter()
-                .find(|v| v.identifier == voice_id)
-                .unwrap();
+    
+        let voice = espeaker::list_voices()
+        .into_iter()
+        .find(|v| v.identifier == voice_id)
+        .unwrap();
+
+        thread::spawn(move || {
+            let mut espeaker = espeaker::Speaker::new();                
             espeaker.set_voice(&voice);
-        }
-        espeaker.params.volume = Some((volume * 100.0).round() as i32);
-        espeaker.params.pitch = Some((pitch * 50.0).round() as i32);
-        espeaker.params.rate = Some((rate * NORMAL_RATE).round() as i32);
-        let task_entry = TaskEntry {
-            _stream,
-            sink,
-            espeaker,
-        };
-        let source = task_entry.espeaker.speak(&s);
-        let source = source.with_callback(move |evt| match evt {
-            espeaker::Event::Start => {
-                block_on(Speaker::speech_start(&owned_ctxt, task_id)).unwrap();
+            espeaker.params.pitch = Some((pitch * 50.0).round() as i32);
+            espeaker.params.rate = Some((rate * NORMAL_RATE).round() as i32);
+
+            let raw_fd = fd.into_raw_fd();
+            // let mut f = File::from(owned_fd);
+            let source = espeaker.speak(&s);
+            let stream_writer = StreamWriter::new(raw_fd);
+            stream_writer.send_stream_header();
+            let mut buffer = [0u8; 2048];
+            let mut index = 0;
+            for (sample, events) in source.iter_audio_and_events() {
+                match events {
+                    None => (),
+                    Some(events) => {
+                        for evt in events {
+                            match evt {
+                                espeaker::Event::Start => (),
+                                espeaker::Event::Word(start, len) => {
+                                    stream_writer.send_event(EventType::Word, start as u32, (start + len) as u32, "");
+                                },
+                                espeaker::Event::Sentence(_) => (),
+                                espeaker::Event::End => ()
+                            }
+                        }
+                        if index > 0 {
+                            stream_writer.send_audio(&buffer[..index-2]);
+                        }
+                        index = 0;
+                    }
+                }
+                [buffer[index], buffer[index+1]] = sample.to_le_bytes();
+                index += 2;
+                if index >= 2048 {
+                    stream_writer.send_audio(&buffer);
+                    index = 0;
+                }
             }
-            espeaker::Event::Word(start, len) => {
-                block_on(Speaker::speech_range_start(
-                    &owned_ctxt,
-                    task_id,
-                    start as u64,
-                    (start + len) as u64,
-                ))
-                .unwrap();
-            }
-            espeaker::Event::Sentence(_) => (),
-            espeaker::Event::End => {
-                block_on(Speaker::speech_end(&owned_ctxt, task_id)).unwrap();
-                thread::spawn(move || {
-                    let mut tasks = TASKS.lock().unwrap();
-                    tasks.remove(&task_key);
-                });
-            }
+            stream_writer.send_audio(&buffer[..index-2]);
         });
-        task_entry.sink.append(source);
-        let mut tasks = TASKS.lock().unwrap();
-        tasks.insert(task_key, task_entry);
     }
-
-    async fn cancel(
-        &self,
-        task_id: u64,
-        #[zbus(header)] header: MessageHeader<'_>,
-        #[zbus(signal_context)] _ctxt: SignalContext<'_>,
-    ) {
-        let sender = String::from(header.sender().unwrap().unwrap().to_owned().as_str());
-        let tasks = TASKS.lock().unwrap();
-        let task_key = Speaker::task_hash(sender.as_str(), task_id);
-
-        match tasks.get(&task_key) {
-            None => (),
-            Some(entry) => entry.sink.stop(),
-        }
-    }
-
-    async fn pause(
-        &self,
-        task_id: u64,
-        #[zbus(header)] header: MessageHeader<'_>,
-        #[zbus(signal_context)] _ctxt: SignalContext<'_>,
-    ) {
-        let sender = String::from(header.sender().unwrap().unwrap().to_owned().as_str());
-        let tasks = TASKS.lock().unwrap();
-        let task_key = Speaker::task_hash(sender.as_str(), task_id);
-
-        match tasks.get(&task_key) {
-            None => (),
-            Some(entry) => entry.sink.pause(),
-        }
-    }
-
-    async fn resume(
-        &self,
-        task_id: u64,
-        #[zbus(header)] header: MessageHeader<'_>,
-        #[zbus(signal_context)] _ctxt: SignalContext<'_>,
-    ) {
-        let sender = String::from(header.sender().unwrap().unwrap().to_owned().as_str());
-        let tasks = TASKS.lock().unwrap();
-        let task_key = Speaker::task_hash(sender.as_str(), task_id);
-
-        match tasks.get(&task_key) {
-            None => (),
-            Some(entry) => entry.sink.play(),
-        }
-    }
-
-    #[dbus_interface(signal)]
-    async fn speech_start(ctxt: &SignalContext<'_>, task_id: u64) -> Result<()>;
-
-    #[dbus_interface(signal)]
-    async fn speech_end(ctxt: &SignalContext<'_>, task_id: u64) -> Result<()>;
-
-    #[dbus_interface(signal)]
-    async fn speech_range_start(
-        ctxt: &SignalContext<'_>,
-        task_id: u64,
-        start: u64,
-        end: u64,
-    ) -> Result<()>;
 }
 
 // Although we use `async-std` here, you can use any async runtime of choice.
